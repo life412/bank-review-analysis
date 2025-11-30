fb046ef55042e0bcda7d41819be8171f4dd13f0f
life412 <151120526+life412@users.noreply.github.com>
2025-11-30 22:42:51 +0300

    Add script to scrape Google Play reviews for banks
    This script scrapes reviews from Google Play for specified banking apps and saves them to a CSV file.

---
+++ scripts/scrape_reviews.py
@@ -0,0 +1,33 @@
+from google_play_scraper import Sort, reviews
+import pandas as pd
+
+apps = {
+    "CBE": "com.cbe.mobilebanking",
+    "BOA": "boa.et.mobilebanking",
+    "Abyssinia": "com.bankofabyssinia.mobilebanking"
+}
+
+all_reviews = []
+
+for bank, app_id in apps.items():
+    result, _ = reviews(
+        app_id,
+        lang='en',
+        country='us',
+        sort=Sort.NEWEST,
+        count=500
+    )
+
+    for r in result:
+        all_reviews.append({
+            "review": r["content"],
+            "rating": r["score"],
+            "date": r["at"],
+            "bank": bank,
+            "source": "Google Play"
+        })
+
+df = pd.DataFrame(all_reviews)
+df.to_csv("data/raw_reviews.csv", index=False)
+
+print("Saved raw reviews!")
